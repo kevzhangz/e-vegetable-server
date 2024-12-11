d376239ede482247664793dc707484eda1e4c791
Kevin <kevzhangz888@gmail.com>
2024-12-11 13:08:52 +0700

    fix: seller login error

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -42,7 +42,7 @@ const signin = async (req, res, next) => {
 
   if(req.body.role == 'seller'){
     let store = await Store.findOne({owner: user._id});
-    userData = {...user._doc, geolocation: store.geolocation, store_id: store.store_id};
+    userData = {...user._doc, geolocation: store.geolocation ?? {}, store_id: store.store_id ?? null};
   } else {
     userData = {...user._doc};
   }
